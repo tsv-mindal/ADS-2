01d894c818493dbf3026838fe72832efa60ed7e6
Polina Plesovskih <78843788+tsv-mindal@users.noreply.github.com>
2021-04-05 22:30:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,67 @@
 
 int countPairs1(int *arr, int len, int value) {
   return 0;
+  kolvo = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if ((arr[i] + arr[j]) == value) 
+        kolvo++;
+    }
+  }
+  return kolvo;        
 }
 
 int countPairs2(int *arr, int len, int value) {
   return 0;
+  int kolvo = 0;
+  int l = 0, r = l -1;
+  while ((r - 1) > l) {
+    int ser = (r + l)/2;
+    if (value >= arr[ser]) 
+      l = ser;
+    else 
+      r = ser;
+  }
+  len = r - 1;
+  for (int i = len; i >= 0; i--) {
+    for (int j = 0; j < i; j++) {
+      if ((arr[i] + arr[j]) == value)
+        kolvo++;
+      if ((arr[i] + arr[j]) > value)
+        break;
+    }
+  }
+  return kolvo;
 }
 
 int countPairs3(int *arr, int len, int value) {
   return 0;
+  int kolvo = 0;
+  int l = 0, r = len - 1;
+  while ((r-1) > l) {
+    int ser = (r + l)/2;
+    if (value >= arr[ser]) 
+      l = ser;
+    else 
+      r = ser;
+  }
+  len = r - 1;
+  for (inr i = 0; i < len; i++) {
+    l = i + 1;
+    r = len - 1;
+    int countN = 0;
+    while (r > l) {
+      int ser = (r + l)/2;
+      if ((value - arr[i]) > arr[ser]))
+        l = ser + 1;
+      else 
+        r = ser;
+    }
+    while ((value - arr[i]) == arr[l])) {
+      countN++;
+      l++;
+    }
+    kolvo = kolvo + countN;
+  }
+  return kolvo;
 }
